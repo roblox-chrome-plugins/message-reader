abed0e991ce6fced1c6515c4007e477871ca739c
Eric Wieser <wieser.eric@gmail.com>
2011-09-25 10:02:48 +0100

    0.0.7 - Working techical preview

---
+++ background.js
@@ -0,0 +1,60 @@
+function Message(data) {
+	this.from = {
+		name: data[0],
+		id: +data[4]
+	};
+	this.title = data[1];
+	this.id = +data[3];
+	this.read = data[5] === 'false';
+	this.date = data[2];
+	if(this.date.match(/\d+:\d+/))
+		this.date = "Today";
+}
+Message.prototype.getURL = function() {
+	return 'http://www.roblox.com/My/PrivateMessage.aspx?MessageID=' + this.id + '&from=Inbox';
+};
+Message.prototype.loadContent = function(callback) {
+	var that = this;
+	if(that.content === undefined) {
+		$.get(that.getURL(), function(html) {
+			var body = $(html).find('#pBody');
+			body.find('br').replaceWith('\n');
+			that.content = body.text().split('------------------------------');
+			console.log(that.content);
+			callback(that.content);
+		});
+	}
+};
+
+function getMessages(callback) {
+	$.get('http://www.roblox.com/My/MessagesHandler.ashx', {
+		page: 1,
+		msgsPerPage: 50,
+		type: 'GetInbox',
+		sort: '[Created] DESC'
+	}, function(data) {
+		var messages = data.Messages.map(function(e) {
+			return new Message(e);	
+		});
+		console.log(messages);
+		callback(messages);
+	});
+
+}
+
+function goTo(url) {
+	chrome.tabs.getAllInWindow(undefined, function (tabs) {
+		var exists = false;
+		$(tabs).each(function () {
+			if (this.url && 0 == this.url.indexOf(url)) {
+				exists = this.id;
+				return;
+			}
+		});
+		
+		if(exists != false)
+			chrome.tabs.update(exists, {selected: true});
+		else
+			chrome.tabs.create({url: url});
+	});
+}
